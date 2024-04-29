a85d8620d7ac9d9a414cb5609c833ca74f37d0c2
amit <amit@ow.com>
2024-04-29 03:55:48 +0000

    add

--- var.vpc.tf
+++ var.vpc.tf
@@ -1,13 +1,3 @@
-variable "vpc_cidr_block" {
-  type    = string
-  default = "10.0.0.0/16"
-}
-
-variable "vpc_name" {
-  type    = string
-  default = "amit.01-vpc"
-}
-
 resource "aws_vpc" "main-vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
